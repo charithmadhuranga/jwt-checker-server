f99225e0fa859fe75c0f894045b5ede00917539d
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-08-08 10:58:11 +0200

    fix(App): Fix startup address binding

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const fastify = require('fastify')({
 
 fastify.register(require('./routes/jwt'));
 
-fastify.listen(process.env.PORT || 3000, (err, address) => {
+fastify.listen(process.env.PORT || 3000, '::', (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
